23d313b49eb3b57f93efd688714b236759e415bf
RaihanArdiann <raihan.ardiann@gmail.com>
2025-01-15 21:25:55 +0700

    feat: add configuration management and user table migration

--- cmd/main.go
+++ cmd/main.go
@@ -1,15 +1,35 @@
 package main
 
 import (
+	"log"
+
 	"github.com/gin-gonic/gin"
+	"github.com/raihan.ardiann/simpleForum/internal/configs"
 	"github.com/raihan.ardiann/simpleForum/internal/handlers/memberships"
 )
 
 func main() {
 	r := gin.Default()
 
+	var (
+		cfg *configs.Config
+	)
+
+	err := configs.Init(
+		configs.WithConfigFolder(
+			[]string{"./internal/configs/"},
+		),
+		configs.WithConfigFile("config"),
+		configs.WithConfigType("yaml"),
+	)
+	if err != nil {
+		log.Fatal("failed to initialize config")
+	}
+	cfg = configs.Get()
+	log.Println("config", cfg)
+
 	membershipsHandler := memberships.NewHandler(r)
 	membershipsHandler.RegisterRoute()
 
-	r.Run(":9999") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	r.Run(cfg.Service.Port) // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
