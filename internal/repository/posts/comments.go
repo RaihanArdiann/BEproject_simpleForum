2b9d1352b7102298ffad486a034507868d00b309
RaihanArdiann <raihan.ardiann@gmail.com>
2025-01-31 02:01:04 +0700

    fix: correct user-id to user_id in CreateComment query and return error on execution failure

--- internal/repository/posts/comments.go
+++ internal/repository/posts/comments.go
@@ -7,10 +7,10 @@ import (
 )
 
 func (r *repository) CreateComment(ctx context.Context, model posts.CommentModel) error {
-	query := `INSERT INTO comments(post_id, user-id, comment_content, created_at, updated_at, created_by, updated_by) VALUES (?, ?, ?, ?, ?, ?, ?)`
+	query := `INSERT INTO comments(post_id, user_id, comment_content, created_at, updated_at, created_by, updated_by) VALUES (?, ?, ?, ?, ?, ?, ?)`
 	_, err := r.db.ExecContext(ctx, query, model.PostID, model.UserID, model.CommentContent, model.CreatedAt, model.UpdatedAt, model.CreatedBy, model.UpdatedBy)
 	if err != nil {
-		return nil
+		return err
 	}
 	return nil
 }
