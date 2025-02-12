21cb4437696f36d76dddc4061e0997354d36da52
RaihanArdiann <raihan.ardiann@gmail.com>
2025-02-13 03:01:25 +0700

    feat: implement refresh token functionality with generation, validation, and middleware support

--- internal/services/memberships/login.go
+++ internal/services/memberships/login.go
@@ -3,34 +3,65 @@ package memberships
 import (
 	"context"
 	"errors"
+	"strconv"
+	"time"
 
 	"github.com/RaihanArdiann/BEproject_simpleForum/internal/model/memberships"
 	"github.com/RaihanArdiann/BEproject_simpleForum/pkg/jwt"
+	tokenUtil "github.com/RaihanArdiann/BEproject_simpleForum/pkg/token"
 	"github.com/rs/zerolog/log"
 	"golang.org/x/crypto/bcrypt"
 )
 
-func (s *service) Login(ctx context.Context, req memberships.LoginRequest) (string, error) {
+func (s *service) Login(ctx context.Context, req memberships.LoginRequest) (string, string, error) {
 	log.Info().Msgf("Attempting to login with email: %s", req.Email)
-	user, err := s.membershipRepo.GetUser(ctx, req.Email, "")
+	user, err := s.membershipRepo.GetUser(ctx, req.Email, "", 0)
 	if err != nil {
 		log.Error().Err(err).Msg("Failed to get user")
-		return "", err
+		return "", "", err
 	}
 	if user == nil {
-		log.Warn().Msg("User not found")
-		return "", errors.New("email not exist")
+		return "", "", errors.New("email not exist")
 	}
 
 	err = bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(req.Password))
 	if err != nil {
-		log.Error().Err(err).Msg("Password comparison failed")
-		return "", errors.New("email or password is invalid")
+		return "", "", errors.New("email or password is invalid")
 	}
+
 	token, err := jwt.CreateToken(user.ID, user.Username, s.cfg.Service.SecretJWT)
 	if err != nil {
 		log.Error().Err(err).Msg("Failed to create token")
-		return "", err
+		return "", "", err
 	}
-	return token, nil
+
+	existingRefreshToken, err := s.membershipRepo.GetRefreshToken(ctx, user.ID, time.Now())
+	if err != nil {
+		log.Error().Err(err).Msg("error get latest refresh token from database")
+		return "", "", err
+	}
+
+	if existingRefreshToken != nil {
+		return token, existingRefreshToken.RefreshToken, nil
+	}
+	refreshToken := tokenUtil.GenerateRefreshToken()
+	if refreshToken == "" {
+		return token, "", errors.New("failed to generate refresh token")
+	}
+
+	err = s.membershipRepo.InsertRefreshToken(ctx, memberships.RefreshTokenModel{
+		UserID:       user.ID,
+		RefreshToken: refreshToken,
+		ExpiredAt:    time.Now().Add(10 * 24 * time.Hour),
+		CreatedAt:    time.Now(),
+		UpdatedAt:    time.Now(),
+		CreatedBy:    strconv.FormatInt(user.ID, 10),
+		UpdatedBy:    strconv.FormatInt(user.ID, 10),
+	})
+	if err != nil {
+		log.Error().Err(err).Msg("error inserting refresh token to database")
+		return token, refreshToken, err
+	}
+
+	return token, refreshToken, nil
 }
